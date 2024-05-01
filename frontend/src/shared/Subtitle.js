3cc26dc72bb050771a30895fec71cbbd38c54a5a
Parvathamabhinav <onlyforfreefire51@gmail.com>
2024-05-02 00:33:43 +0530

    Changed for testing

--- frontend/src/shared/Subtitle.js
+++ frontend/src/shared/Subtitle.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const subtitle = ({subtitle}) => {
   return (
-    <h3 className='section_subtitle'></h3>
-  )
+    <h3 className='section_subtitle'>Subtitle</h3>
+  );
 }
 
-export default subtitle
+export default subtitle;
